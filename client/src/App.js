347110420376a7263dc624bf8f8dcb9453d11564
sahil70526 <sahilsingh70526@gmail.com>
2022-09-02 20:36:52 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
+import './Style/modified.css';
+import Home from "./homeComponents/home";
+import Navbar from './homeComponents/Navbar';
+import About from './homeComponents/about';
+import Login from './homeComponents/Login';
+import Signup from './homeComponents/Signup';
+import Authenticate from './Authenticate';
+
+import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer';
+import Sort from "./sortComponents/sort";
+import Queen from "./queenComponents/queen";
+import ConvexHull from "./convexHullComponents/convexHull";
+
+import BinarySearch from './binarySearchComponent/BinarySearch';
+import RecursiveSort from "./recursiveSortComponents/recursiveSort";
+import { createContext } from 'react';
+export const Url = createContext();
+
+    function App(){
+        return (
+      <Url.Provider value={'https://algoviz-vbspu.herokuapp.com/api'}>
+            <BrowserRouter>
+            <Navbar/>
+                <Routes>
+                <Route path='/pathfinder' element={<PathfindingVisualizer/>}/>
+                    {/* <Route path='/prime' component={Seive}/> */}
+                    <Route path='/sort' element={<Sort/>}/>
+                    <Route path='/nqueen' element={<Queen/>}/>
+                    <Route path='/convexhull' element={<ConvexHull/>}/>
+                    <Route path='/binarysearch' element={<BinarySearch/>}/>
+                    <Route path='/recursivesort' element={<RecursiveSort/>}/>  
+                    {/* <Route path='/towerOfHanoi' element={<towerOfHanoi/>}/> */}
+                    <Route path='/Authenticate' element={<Authenticate/>}/>
+                    <Route path='/login' element={<Login/>}/>
+                    <Route path='/signup' element={<Signup/>}/>
+                    <Route path='/about' element={<About/>}/>
+                    <Route path='/' element={<Home/>}/>
+                </Routes>
+         </BrowserRouter>
+      </Url.Provider>
+        );
+    }
+
+
+export default App;
